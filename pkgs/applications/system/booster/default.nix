63a5675fa4e29df65d92789272164eaeac6b5a59
Colin Arnott <colin@urandom.co.uk>
2023-01-12 02:25:53 +0000

    booster: pin exec targets and add manpage
    We use a number of libraries and a configuration path that are linked
    during build, which also have binaries that get called at runtime.
    Allowing these to not point to the same backing derivations is a great
    way to introduce hard to reproduce failures. As such, we have patched
    the source to exec fully qualified paths, as was previously done for
    /usr/bin/fsck.
    
    While were were here, there is also a nice manpage that we failed to
    include before, so that has been resolved with help from roff. As well,
    there are a number of /usr/something defaults that have been pointed to
    their correct /nix/store locations.

--- pkgs/applications/system/booster/default.nix
+++ pkgs/applications/system/booster/default.nix
@@ -1,17 +1,26 @@
 { bash
 , binutils
 , buildGoModule
+, cpio
 , fetchFromGitHub
+, gcc
+, go
+, gzip
+, installShellFiles
 , kbd
 , lib
 , libfido2
+, linux
 , lvm2
 , lz4
-, makeWrapper
 , mdadm
+, ronn
+, swtpm-tpm2
 , unixtools
+, usbutils
 , xz
 , zfs
+, zstd
 }:
 
 buildGoModule rec {
@@ -27,25 +36,65 @@ buildGoModule rec {
 
   vendorHash = "sha256-GD+nsT4/Y2mTF+ztOC3N560BY5+QSfsPrXZ+dJYtzAw=";
 
+  # templatise exec calls for substituteInPlace
+  patches = [./exec.patch];
+
+  # runtimeInputs = [ bash binutils kbd libfido2 lvm2 mdadm zfs ];
   postPatch = ''
-    substituteInPlace init/main.go --replace "/usr/bin/fsck" "${unixtools.fsck}/bin/fsck"
+    mv docs/manpage.md docs/booster.md
+    substituteInPlace generator/*.go init/*.go \
+      --subst-var-by "bash" "${bash}/bin/bash" \
+      --subst-var-by "cpio" "${cpio}/bin/cpio" \
+      --subst-var-by "fido2-assert" "${libfido2}/bin/fido2-assert" \
+      --subst-var-by "fsck" "${unixtools.fsck}/bin/fsck" \
+      --subst-var-by "gcc" "${gcc}/bin/gcc" \
+      --subst-var-by "go" "${go}/bin/go" \
+      --subst-var-by "gzip" "${gzip}/bin/gzip" \
+      --subst-var-by "loadkeys" "${kbd}/bin/loadkeys" \
+      --subst-var-by "lsusb" "${usbutils}/bin/lsusb" \
+      --subst-var-by "lvm" "${lvm2}/bin/lvm" \
+      --subst-var-by "lz4" "${lz4}/bin/lz4" \
+      --subst-var-by "mdadm" "${mdadm}/bin/mdadm" \
+      --subst-var-by "modules" "${linux.dev}/lib/modules" \
+      --subst-var-by "out" "${placeholder "out"}" \
+      --subst-var-by "setfont" "${kbd}/bin/setfont" \
+      --subst-var-by "strip" "${gcc}/bin/strip" \
+      --subst-var-by "swtpm" "${swtpm-tpm2}/bin/swtpm" \
+      --subst-var-by "xz" "${xz}/bin/xz" \
+      --subst-var-by "zfs" "${zfs}/bin/zfs" \
+      --subst-var-by "zpool" "${zfs}/bin/zpool" \
+      --subst-var-by "zstd" "${zstd}/bin/zstd"
+  '';
+
+  postBuild = ''
+    ${ronn}/bin/ronn docs/booster.md
   '';
 
   # integration tests are run against the current kernel
   doCheck = false;
 
   nativeBuildInputs = [
+    installShellFiles
     kbd
     lz4
-    makeWrapper
     xz
   ];
 
-  postInstall = let
-    runtimeInputs = [ bash binutils kbd libfido2 lvm2 mdadm zfs ];
-  in ''
-    wrapProgram $out/bin/generator --prefix PATH : ${lib.makeBinPath runtimeInputs}
-    wrapProgram $out/bin/init --prefix PATH : ${lib.makeBinPath runtimeInputs}
+  ldflags = [
+    "-s"
+    "-w"
+    "-X github.com/anatol/booster/generator.consolefontsDir=${kbd}/share/consolefonts/"
+    "-X github.com/anatol/booster/generator.firmwareDir=${linux.dev}/lib/firmware"
+    "-X github.com/anatol/booster/generator.imageModulesDir=${linux.dev}/lib/modules"
+    "-X github.com/anatol/booster/init.imageModulesDir=${linux.dev}/lib/modules"
+  ];
+
+  postInstall = ''
+    mkdir $out/etc/
+    touch $out/etc/booster.yaml
+    mv $out/bin/{generator,booster}
+    installManPage docs/booster.1
+    installShellCompletion --bash --name booster.bash contrib/completion/bash
   '';
 
   meta = with lib; {
